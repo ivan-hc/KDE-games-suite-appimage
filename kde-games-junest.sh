382ba23d3b004a9665913659c4d8a37e31b2eaf9
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-12-29 00:11:24 +0100

    Update kde-games-junest.sh
    - add hardware acceleration

--- kde-games-junest.sh
+++ kde-games-junest.sh
@@ -143,15 +143,55 @@ export UNION_PRELOAD=$HERE
 export JUNEST_HOME=$HERE/.junest
 export PATH=$PATH:$HERE/.local/share/junest/bin
 
-if test -f /etc/resolv.conf; then ETC_RESOLV=' --bind /etc/resolv.conf /etc/resolv.conf '; fi
-if test -d /media; then MNT_MEDIA_DIR=' --bind /media /media '; fi
-if test -d /mnt; then MNT_DIR=' --bind /mnt /mnt '; fi
-if test -d /opt; then OPT_DIR=' --bind /opt /opt '; fi
-if test -d /run/user; then USR_LIB_LOCALE_DIR=' --bind /usr/lib/locale /usr/lib/locale '; fi
-if test -d /usr/share/fonts; then USR_SHARE_FONTS_DIR=' --bind /usr/share/fonts /usr/share/fonts '; fi
-if test -d /usr/share/themes; then USR_SHARE_THEMES_DIR=' --bind /usr/share/themes /usr/share/themes '; fi
 
-BINDS=" $ETC_RESOLV $MNT_MEDIA_DIR $MNT_DIR $OPT_DIR $USR_LIB_LOCALE_DIR $USR_SHARE_FONTS_DIR $USR_SHARE_THEMES_DIR "
+[ -z "$NVIDIA_ON" ] && NVIDIA_ON=1
+if [ "$NVIDIA_ON" = 1 ]; then
+  DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
+  CONTY_DIR="${DATADIR}/Conty/overlayfs_shared"
+  CACHEDIR="${XDG_CACHE_HOME:-$HOME/.cache}"
+  [ -f /sys/module/nvidia/version ] && nvidia_driver_version="$(cat /sys/module/nvidia/version)"
+  [ -f "${CONTY_DIR}"/nvidia/current-nvidia-version ] && nvidia_driver_conty="$(cat "${CONTY_DIR}"/nvidia/current-nvidia-version)"
+  if [ "${nvidia_driver_version}" != "${nvidia_driver_conty}" ]; then
+     if command -v curl >/dev/null 2>&1; then
+        if ! curl --output /dev/null --silent --head --fail https://github.com 1>/dev/null; then
+          notify-send "You are offline, cannot use Nvidia drivers"
+        else
+          notify-send "Configuring Nvidia drivers for this AppImage..."
+          mkdir -p "${CACHEDIR}" && cd "${CACHEDIR}" || exit 1
+          curl -Ls "https://raw.githubusercontent.com/ivan-hc/ArchImage/main/nvidia-junest.sh" > nvidia-junest.sh
+          chmod a+x ./nvidia-junest.sh && ./nvidia-junest.sh
+        fi
+     else
+        notify-send "Missing \"curl\" command, cannot use Nvidia drivers"
+        echo "You need \"curl\" to download this script"
+     fi
+  fi
+  [ -d "${CONTY_DIR}"/up/usr/bin ] && export PATH="${PATH}":"${CONTY_DIR}"/up/usr/bin:"${PATH}"
+  [ -d "${CONTY_DIR}"/up/usr/lib ] && export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}":"${CONTY_DIR}"/up/usr/lib:"${LD_LIBRARY_PATH}"
+  [ -d "${CONTY_DIR}"/up/usr/share ] && export XDG_DATA_DIRS="${XDG_DATA_DIRS}":"${CONTY_DIR}"/up/usr/share:"${XDG_DATA_DIRS}"
+fi
+
+BINDS=" --dev-bind /dev /dev \
+	--ro-bind /sys /sys \
+	--bind-try /tmp /tmp \
+	--proc /proc \
+	--ro-bind-try /etc/resolv.conf /etc/resolv.conf \
+	--ro-bind-try /etc/hosts /etc/hosts \
+	--ro-bind-try /etc/nsswitch.conf /etc/nsswitch.conf \
+	--ro-bind-try /etc/passwd /etc/passwd \
+	--ro-bind-try /etc/group /etc/group \
+	--ro-bind-try /etc/machine-id /etc/machine-id \
+	--ro-bind-try /etc/asound.conf /etc/asound.conf \
+	--ro-bind-try /etc/localtime /etc/localtime \
+	--bind-try /media /media \
+	--bind-try /mnt /mnt \
+	--bind-try /opt /opt \
+	--bind-try /run/media /run/media \
+	--bind-try /usr/lib/locale /usr/lib/locale \
+	--bind-try /usr/share/fonts /usr/share/fonts \
+	--bind-try /usr/share/themes /usr/share/themes \
+	--bind-try /var /var \
+	"
 
 case $1 in
 '')
@@ -467,11 +507,14 @@ mkdir -p ./"$APP".AppDir/.junest/media
 mkdir -p ./"$APP".AppDir/.junest/usr/lib/locale
 mkdir -p ./"$APP".AppDir/.junest/usr/share/fonts
 mkdir -p ./"$APP".AppDir/.junest/usr/share/themes
+mkdir -p ./"$APP".AppDir/.junest/run/media
 mkdir -p ./"$APP".AppDir/.junest/run/user
+rm -f ./"$APP".AppDir/.junest/etc/localtime && touch ./"$APP".AppDir/.junest/etc/localtime
+[ ! -f ./"$APP".AppDir/.junest/etc/asound.conf ] && touch ./"$APP".AppDir/.junest/etc/asound.conf
 
 # CREATE THE APPIMAGE
 if test -f ./*.AppImage; then
 	rm -R -f ./*archimage*.AppImage
 fi
 ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./$APP.AppDir
-mv ./*AppImage ./KDE-GAMES-SUITE_"$VERSION"-archimage3.4.2-1-x86_64.AppImage
+mv ./*AppImage ./KDE-GAMES-SUITE_"$VERSION"-archimage4-x86_64.AppImage
